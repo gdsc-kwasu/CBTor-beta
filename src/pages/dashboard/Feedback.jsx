554a232d6a006d79232dece7d7d6c0f1a2ec0399
Caleb Olojo <belac335@gmail.com>
2021-01-10 23:16:26 +0100

    feat: add feedback, wallet scaffold

--- src/pages/dashboard/Feedback.jsx
+++ src/pages/dashboard/Feedback.jsx
@@ -1,9 +1,23 @@
 /** @format */
 
 import React from "react";
+import Button from "../../components/Buttons";
 
 const Feedback = () => {
-  return <>Feedback</>;
+  return (
+    <section>
+      <p className="mb-3 text-dark font-weight-bold">Feedback</p>
+      <div className="">
+        <textarea
+          rows="13"
+          cols="75"
+          className="mb-3 form-control col-12 col-md-9 col-xl-9"
+          defaultValue="This is my feedback..."
+        ></textarea>
+      </div>
+      <Button className="btn-primary">Send Feedback</Button>
+    </section>
+  );
 };
 
 export default Feedback;
